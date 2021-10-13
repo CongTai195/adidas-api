a05ebd93587a401734f3faab87173ac0a6f114c9
thanghokdps <thanghorit@gmail.com>
2021-10-13 15:10:30 +0700

    done migrations

---
+++ database/migrations/2021_10_13_070213_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->tinyInteger('status')->default(0);
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('user_name')->notnull();
+            $table->string('user_email')->notnull();
+            $table->string('user_phone_number')->notnull();
+            $table->integer('view')->default(0);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
--- database/migrations/2021_10_13_072931_create_warehouse_table.php
+++ database/migrations/2021_10_13_072931_create_warehouse_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateWarehouseTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('warehouse', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('user_name')->unique();
-            $table->string('password');
-            $table->string('address')->nullable();
-            $table->string('email')->unique();
-            $table->string('phone_number')->unique();
+            $table->bigInteger('product_id')->unsigned();
+            $table->integer('size')->notnull();
+            $table->string('color')->notnull();
+            $table->integer('quantily')->default(0);
+            $table->foreign('product_id')->references('id')->on('products');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('warehouse');
     }
 }
