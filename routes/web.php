ea0535345719b653ab2eb7385c68d8b9258550c6
thanghokdps <thanghorit@gmail.com>
2021-11-21 14:02:46 +0700

    load image

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/product/{id}/{name}', 'Controller@viewImage')->name('viewImage');
